6566ae2f870f427e72229a76188678a789456fcf
Sankalp Tiwari <41205888+SankalpSTiwari@users.noreply.github.com>
2023-11-07 18:46:33 -0800

    Implemented User API Routes & JWT Authentication

--- server.js
+++ server.js
@@ -6,11 +6,20 @@ const app = express()
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({extended: false}));
+
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 app.listen(PORT, () => {
   console.log(`Example app listening on PORT ${PORT}`)
