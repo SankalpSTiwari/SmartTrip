83f733abccb2007ac336da31a689322a1c9cfda7
Sayali Nilangekar <113153528+SayaliNilangekar@users.noreply.github.com>
2023-11-09 22:53:04 -0800

    Front end setup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React, { Fragment } from 'react';
+
+
+const App = () => {
+  return (
+    <div className="App">
+    <Fragment>
+      <h1>App</h1>
+    </Fragment>
+    </div>
+  ); 
+}
+
+export default App;
