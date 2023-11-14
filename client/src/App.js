19b6665d9649050dd58e0921b1ac1399157f15e5
Sayali Nilangekar <113153528+SayaliNilangekar@users.noreply.github.com>
2023-11-14 13:14:42 -0800

    Add alerts

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,18 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
-
+import Alert from './components/layout/Alert';
 import './App.css';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => (
+  <Provider store={store}>
   <Router>
     <Fragment>
       <Navbar />
+      <Alert />
       <Routes>
         <Route exact path='/' element={<Landing />} />
         <Route exact path='/register' element={<Register />} />
@@ -18,6 +23,7 @@ const App = () => (
       </Routes>
     </Fragment>
   </Router>
+  </Provider> 
 );
 
 export default App;
